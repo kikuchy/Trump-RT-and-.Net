bd697c48b79b41578427bfb54b56397ec6f699af
Hiroshi kikuchi <kikuchy.h@gmail.com>
2012-10-01 21:24:07 +0900

    手札のテストクラスを作成。

---
+++ TrumpUnitTest/HandTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrumpUnitTest
+{
+    [TestClass]
+    public class HandTest
+    {
+        [TestMethod]
+        public void InitializeTest()
+        {
+        }
+    }
+}
